41cbc98a6db1e3025ce6ffb3c8d8c178f9001214
CoulterStutz <coultercash@gmail.com>
2024-05-26 14:24:20 -0600

    Update __init__.py

--- Nifty/__init__.py
+++ Nifty/__init__.py
@@ -1,2 +1,7 @@
 from .opensea import OpenSea
-from .EVM import EVM
+from .EVM import EVM, chains
+
+__all__ = ["EVM", "chains", "OpenSea"]
+__version__ = '1.0.0'
+__author__ = 'Coulter C. Stutz'
+__email__ = 'coulterstutz@gmail.com'
