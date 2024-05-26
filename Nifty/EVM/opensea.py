32d48e89e87c4fd0800aec0352b52b5e08cb7431
CoulterStutz <coultercash@gmail.com>
2024-05-26 12:13:53 -0600

    Update opensea.py

--- Nifty/EVM/opensea.py
+++ Nifty/EVM/opensea.py
@@ -18,6 +18,18 @@ class OpenSea:
         else:
             response.raise_for_status()
 
+    def get_traits(self, collection_slug):
+        url = f"https://api.opensea.io/api/v2/collection/{collection_slug}/traits"
+        headers = {
+            "Accept": "application/json",
+            "X-API-KEY": self.api_key
+        }
+        response = requests.get(url, headers=headers)
+        if response.status_code == 200:
+            return response.json()
+        else:
+            response.raise_for_status()
+
     def get_collection(self):
         url = f"https://api.opensea.io/api/v2/collections/{self.collection_slug}"
         headers = {
