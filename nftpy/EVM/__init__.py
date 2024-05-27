962a853c7e598b8e614f1cb7f50268f5c5b2bee2
CoulterStutz <coultercash@gmail.com>
2024-05-27 14:48:55 -0600

    [IMPORTATION ERRORS]: Fix Importation Errors

--- nftpy/EVM/__init__.py
+++ nftpy/EVM/__init__.py
@@ -1,5 +1,6 @@
 from .nft import NFT
 from .abi import ABI
 from .chains import Chains
+from .wallet import NFTWallet
 
-__all__ = ['NFT', 'ABI', 'Chains']
+__all__ = ['NFT', 'ABI', 'Chains', "NFTWallet"]
--- nftpy/__init__.py
+++ nftpy/__init__.py
@@ -1,7 +1,7 @@
 from .opensea import OpenSea, OpenSeaChain
-from .EVM import NFT, Chains, ABI
+from .EVM import NFT, Chains, ABI, NFTWallet
 
-__all__ = ["EVM", "OpenSea", "NFT", "OpenSeaChain"]
+__all__ = ["EVM", "OpenSea", "NFT", "OpenSeaChain", "NFTWallet", "Chains", "ABI"]
 __version__ = '1.0.3'
 __author__ = 'Coulter C. Stutz'
 __email__ = 'coulterstutz@gmail.com'
