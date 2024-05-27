9e85c60714268a34fe4070990714468c14bcc09f
CoulterStutz <coultercash@gmail.com>
2024-05-27 14:32:47 -0600

    [PROJECT]: Change Version

--- nftpy/__init__.py
+++ nftpy/__init__.py
@@ -2,6 +2,6 @@ from .opensea import OpenSea, OpenSeaChain
 from .EVM import NFT, Chains, ABI
 
 __all__ = ["EVM", "OpenSea", "NFT", "OpenSeaChain"]
-__version__ = '1.0.0'
+__version__ = '1.0.3'
 __author__ = 'Coulter C. Stutz'
 __email__ = 'coulterstutz@gmail.com'
