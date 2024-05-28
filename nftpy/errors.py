9f6a39a200dbde38cda0f82b2a70caeefba443e1
CoulterStutz <coultercash@gmail.com>
2024-05-27 23:31:22 -0600

    [PROJECT]: Rewrote errors.py

--- nftpy/errors.py
+++ nftpy/errors.py
@@ -1,40 +1,40 @@
 class NFTException(Exception):
-    """Base class for exceptions in NFT class."""
+    """Base class for exceptions in the NFT class."""
     pass
 
 class InvalidRPCURL(NFTException):
     """Raised when an invalid RPC URL is provided."""
-    def __init__(self,url, chain=None):
-        if chain is None:
-            self.message = f"Invalid RPC URL provided! {url}"
-        else:
-            self.message = f"Invalid RPC URL provided for {chain}! {url}"
-        super().__init__(self.message)
+    def __init__(self, url, chain=None):
+        super().__init__()
 
 class ContractFunctionFailed(NFTException):
     """Raised when a contract function call fails."""
     def __init__(self, function_name):
-        self.message = f"Contract function '{function_name}' call failed."
-        super().__init__(self.message)
+        super().__init__()
+
+class TransactionGasError(NFTException):
+    """Raised when a transaction fails due to insufficient gas."""
+    pass
+
+class TransactionBalanceError(NFTException):
+    """Raised when a transaction fails due to insufficient balance."""
+    pass
 
 class OpenSeaException(Exception):
-    """Base class for exceptions in OpenSea class."""
+    """Base class for exceptions in the OpenSea class."""
     pass
 
 class MissingSlug(OpenSeaException):
     """Raised when a collection slug is required but not provided."""
     def __init__(self):
-        self.message = "Collection slug must be provided."
-        super().__init__(self.message)
+        super().__init__()
 
 class MissingChain(OpenSeaException):
     """Raised when a chain is required but not provided."""
     def __init__(self):
-        self.message = "Chain must be provided."
-        super().__init__(self.message)
+        super().__init__()
 
 class APIRequestFailed(OpenSeaException):
     """Raised when an API request fails."""
     def __init__(self, status_code):
-        self.message = f"API request failed with status code {status_code}."
-        super().__init__(self.message)
+        super().__init__()
