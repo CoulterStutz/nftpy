71d3540a3db52dd8501d493c3ca39183be51989e
CoulterStutz <coultercash@gmail.com>
2024-05-28 20:57:03 -0600

    Update __init__.py

--- nftpy/__init__.py
+++ nftpy/__init__.py
@@ -2,6 +2,6 @@ from .opensea import OpenSea, OpenSeaChain
 from .EVM import NFT, Chains, ABI, NFTWallet
 
 __all__ = ["EVM", "OpenSea", "NFT", "OpenSeaChain", "NFTWallet", "Chains", "ABI"]
-__version__ = '1.1.3'
+__version__ = '1.1.4'
 __author__ = 'Coulter C. Stutz'
 __email__ = 'coulterstutz@gmail.com'
