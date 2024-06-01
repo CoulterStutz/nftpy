8dcb5af7d86a3ccaba452a61d604ef3167f731eb
CoulterStutz <coultercash@gmail.com>
2024-06-01 01:52:51 -0600

    [RARIBLE]: Add Into __init__.py

--- nftpy/__init__.py
+++ nftpy/__init__.py
@@ -1,7 +1,9 @@
 from .opensea import OpenSea, OpenSeaChain
-from .EVM import NFT, Chains, ABI, NFTWallet, Chain
+from .rarible import Rarible, RaribleChain
+from .EVM import NFT, NFTWallet, ABI, Chains, Chain
 
-__all__ = ["EVM", "OpenSea", "NFT", "OpenSeaChain", "NFTWallet", "Chains", "ABI", "Chain"]
+__all__ = ["EVM", "NFT", "NFTWallet", "ABI", "Chains", "Chain", "OpenSea", "OpenSeaChain", "Rarible", "RaribleChain"]
+
 __version__ = '1.2.0'
 __author__ = 'Coulter C. Stutz'
 __email__ = 'coulterstutz@gmail.com'
