e85b28c02f226c36935da64a6607c588eeaa8363
CoulterStutz <coultercash@gmail.com>
2024-06-01 02:28:30 -0600

    [PROJECT]: UPDATE VERSION CHANGES

--- nftpy/rarible.py
+++ nftpy/rarible.py
@@ -1,4 +1,5 @@
-from enum import Enum
+from enum import En
+
 import requests
 
 
@@ -73,7 +74,7 @@ class Rarible:
         response.raise_for_status()
         return response.json()
 
-    def get_items_by_creator(creator: str):
+    def get_items_by_creator(self, creator: str):
         """
         Get items created by a specific creator.
 
@@ -89,7 +90,7 @@ class Rarible:
         response.raise_for_status()
         return response.json()
 
-    def get_items_by_collection(collection: str):
+    def get_items_by_collection(self, collection: str):
         """
         Get items in a specific collection.
 
@@ -104,7 +105,7 @@ class Rarible:
         response = requests.get(url, params=params)
         response.raise_for_status()
         return response.json()
-    def query_traits(collection: str):
+    def query_traits(self, collection: str):
         """
         Query traits of items in a specific collection.
 
@@ -120,7 +121,7 @@ class Rarible:
         response.raise_for_status()
         return response.json()
 
-    def get_lazy_item_by_id(item_id: str):
+    def get_lazy_item_by_id(self, item_id: str):
         """
         Get a lazy item by item ID.
 
@@ -134,7 +135,7 @@ class Rarible:
         response = requests.get(url)
         response.raise_for_status()
         return response.json()
-    def burn_lazy_item(item_id: str):
+    def burn_lazy_item(self, item_id: str):
         """
         Burn a lazy item by item ID.
 
@@ -149,7 +150,7 @@ class Rarible:
         response.raise_for_status()
         return response.json()
 
-    def get_ownership_by_id(ownership_id: str):
+    def get_ownership_by_id(self, ownership_id: str):
         """
         Get ownership details by ownership ID.
 
@@ -164,7 +165,7 @@ class Rarible:
         response.raise_for_status()
         return response.json()
 
-    def get_ownerships_by_ids(ownership_ids: list):
+    def get_ownerships_by_ids(self, ownership_ids: list):
         """
         Get multiple ownerships details by their IDs.
 
@@ -180,7 +181,7 @@ class Rarible:
         response.raise_for_status()
         return response.json()
 
-    def get_ownerships_by_collection(collection: str):
+    def get_ownerships_by_collection(self, collection: str):
         """
         Get ownerships in a specific collection.
 
@@ -196,7 +197,7 @@ class Rarible:
         response.raise_for_status()
         return response.json()
 
-    def get_ownerships_by_item(item_id: str):
+    def get_ownerships_by_item(self, item_id: str):
         """
         Get ownerships for a specific item.
 
@@ -212,7 +213,7 @@ class Rarible:
         response.raise_for_status()
         return response.json()
 
-    def get_collections_with_owned_items(owner: str):
+    def get_collections_with_owned_items(self, owner: str):
         """
         Get collections with items owned by a specific owner.
 
@@ -228,9 +229,7 @@ class Rarible:
         response.raise_for_status()
         return response.json()
 
-    import requests
-
-    def get_order_by_id(order_id: str):
+    def get_order_by_id(self, order_id: str):
         """
         Get order details by order ID.
 
@@ -245,7 +244,7 @@ class Rarible:
         response.raise_for_status()
         return response.json()
 
-    def get_order_by_id(order_id: str):
+    def get_order_by_id(self, order_id: str):
         """
         Get order details by order ID.
 
@@ -260,7 +259,7 @@ class Rarible:
         response.raise_for_status()
         return response.json()
 
-    def get_validated_order_by_id(order_id: str):
+    def get_validated_order_by_id(self, order_id: str):
         """
         Get validated order details by order ID.
 
@@ -275,3 +274,84 @@ class Rarible:
         response.raise_for_status()
         return response.json()
 
+    def get_orders_by_ids(self, order_ids: list):
+        """
+        Get multiple orders details by their IDs.
+
+        Args:
+            order_ids (list): A list of order IDs.
+
+        Returns:
+            dict: Details of the orders.
+        """
+        url = f"https://api.rarible.org/v0.1/orders/byIds"
+        params = {"ids": ",".join(order_ids)}
+        response = requests.get(url, params=params)
+        response.raise_for_status()
+        return response.json()
+
+    def get_orders_by_ids(self, order_ids: list):
+        """
+        Get multiple orders details by their IDs.
+
+        Args:
+            order_ids (list): A list of order IDs.
+
+        Returns:
+            dict: Details of the orders.
+        """
+        url = f"https://api.rarible.org/v0.1/orders/byIds"
+        params = {"ids": ",".join(order_ids)}
+        response = requests.get(url, params=params)
+        response.raise_for_status()
+        return response.json()
+
+    def get_orders_all(self):
+        """
+        Get all orders.
+
+        Returns:
+            dict: Details of all orders.
+        """
+        url = f"https://api.rarible.org/v0.1/orders/all"
+        response = requests.get(url)
+        response.raise_for_status()
+        return response.json()
+
+    def get_all_sync(self):
+        url = f"{self.base_url}/orders/all/sync"
+        response = requests.get(url)
+        response.raise_for_status()
+        return response.json()
+
+    def get_sell_orders_by_maker(self, maker: str):
+        """
+        Get sell orders by maker.
+
+        Args:
+            maker (str): The address of the maker.
+
+        Returns:
+            dict: Details of the sell orders by the specified maker.
+        """
+        url = f"{self.base_url}/orders/sell/byMaker"
+        params = {"maker": maker}
+        response = requests.get(url, params=params)
+        response.raise_for_status()
+        return response.json()
+
+    def get_sell_orders_by_item(self, item_id: str):
+        """
+        Get sell orders by item.
+
+        Args:
+            item_id (str): The ID of the item.
+
+        Returns:
+            dict: Details of the sell orders for the specified item.
+        """
+        url = f"{self.base_url}/orders/sell/byItem"
+        params = {"itemId": item_id}
+        response = requests.get(url, params=params)
+        response.raise_for_status()
+        return response.json()
