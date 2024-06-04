e60364af40b81f6e4d91134b0b08e7d7ae827d01
CoulterStutz <coultercash@gmail.com>
2024-06-04 12:23:04 -0600

    [RARIBLE]: Create Collection Class

--- nftpy/__init__.py
+++ nftpy/__init__.py
@@ -1,8 +1,8 @@
 from .opensea import OpenSea, OpenSeaChain
-from .rarible import Rarible, RaribleChain, Collection
+from .rarible import Rarible, RaribleChain, RaribleCollection
 from .EVM import NFT, NFTWallet, ABI, Chains, Chain
 
-__all__ = ["EVM", "NFT", "NFTWallet", "ABI", "Chains", "Chain", "OpenSea", "OpenSeaChain", "Rarible", "RaribleChain", "Collection"]
+__all__ = ["EVM", "NFT", "NFTWallet", "ABI", "Chains", "Chain", "OpenSea", "OpenSeaChain", "Rarible", "RaribleChain", "RaribleCollection"]
 
 __version__ = '1.2.0b'
 __author__ = 'Coulter C. Stutz'
