f2f106cdbc5d871cfed4a9d02624bd19930c0421
CoulterStutz <coultercash@gmail.com>
2024-06-04 15:51:34 -0600

    [PROJECT]: FIX PACKAGE INSTALLATION ERROR

---
+++ setup.py
@@ -0,0 +1,47 @@
+import os
+from setuptools import setup, find_packages
+
+def get_version():
+    version = {}
+    with open(os.path.join("nftpy", "__init__.py")) as fp:
+        exec(fp.read(), version)
+    return version.get('__version__', '0.0.1')
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="nftpy",
+    version=get_version(),
+    author="CoulterStutz",
+    author_email="coultercash@gmail.com",
+    description="A NFT specific Python library for easy NFT integration in Python",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/CoulterStutz/nftpy",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.9',
+    install_requires=[
+        "web3>=6.2.0,<7.0.0",
+        "requests>=2.28.1,<3.0.0",
+        "aiohttp==3.9.5",
+        "eth-abi>=4.0.0",
+        "eth-account<0.13,>=0.8.0",
+        "eth-hash>=0.5.1",
+        "eth-typing!=4.2.0,>=3.0.0",
+        "eth-utils>=2.1.0",
+        "hexbytes<0.4.0,>=0.1.0",
+        "jsonschema>=4.0.0",
+        "lru-dict<1.3.0,>=1.1.6",
+    ],
+    extras_require={
+        "dev": [
+            "twine>=5.1.0"
+        ]
+    },
+)
