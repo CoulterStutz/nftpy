baa5d83badf515a7b0d84c34ed9b9f2c32edf333
CoulterStutz <coultercash@gmail.com>
2024-06-04 12:34:54 -0600

    [OPENSEA]: CREATE MODULE

--- nftpy/__init__.py
+++ nftpy/__init__.py
@@ -1,8 +1,6 @@
-from .opensea import OpenSea, OpenSeaChain
-from .rarible import Rarible, RaribleChain, RaribleCollection
 from .EVM import NFT, NFTWallet, ABI, Chains, Chain
-
-__all__ = ["EVM", "NFT", "NFTWallet", "ABI", "Chains", "Chain", "OpenSea", "OpenSeaChain", "Rarible", "RaribleChain", "RaribleCollection"]
+from .
+__all__ = ["EVM"]
 
 __version__ = '1.2.0b'
 __author__ = 'Coulter C. Stutz'
---
+++ nftpy/opensea/__init__.py
@@ -0,0 +1,3 @@
+from .opensea import OpenSea, OpenSeaChain
+from .collection import OpenSeaCollection
+from .wallet import OpenSeaWallet
