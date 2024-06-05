405d02c2bd74ad5459180e3a28d320b9719f80fd
CoulterStutz <coultercash@gmail.com>
2024-06-04 23:21:59 -0600

    [MINTABLE]: Finish up module; reflect version name

--- nftpy/__init__.py
+++ nftpy/__init__.py
@@ -2,9 +2,9 @@ from .EVM import NFT, NFTWallet, ABI, Chains, Chain
 from .OpenSea import OpenSea, OpenSeaChain, OpenSeaWallet, OpenSeaCollection
 from .Rarible import Rarible, RaribleChain, RaribleCollection, RaribleWallet
 from .Mintable import Mintable, MintableChain
-__all__ = ["EVM", "OpenSea", "Rarible"]
+__all__ = ["EVM", "OpenSea", "Rarible", "Mintable"]
 
 __name__ = "nftpy"
-__version__ = '1.2.1a'
+__version__ = '1.2.1'
 __author__ = 'Coulter C. Stutz'
 __email__ = 'coulterstutz@gmail.com'
