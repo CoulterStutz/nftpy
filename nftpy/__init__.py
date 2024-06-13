51f1040453eb60b245cf241d71d60f736b058ad7
CoulterStutz <coultercash@gmail.com>
2024-06-13 14:02:02 -0600

    [LOOKSRARE]: Import

--- nftpy/__init__.py
+++ nftpy/__init__.py
@@ -3,7 +3,7 @@ from .OpenSea import OpenSea, OpenSeaChain, OpenSeaWallet, OpenSeaCollection
 from .Rarible import Rarible, RaribleChain, RaribleCollection, RaribleWallet
 from .Mintable import Mintable, MintableChain
 from .LooksRare import LooksRareChain, LooksRareAPI
-__all__ = ["EVM", "OpenSea", "Rarible", "Mintable"]
+__all__ = ["EVM", "OpenSea", "Rarible", "Mintable", "LooksRare"]
 
 __name__ = "nftpy"
 __version__ = '1.2.2a1'
