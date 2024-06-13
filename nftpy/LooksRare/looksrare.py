604840364b44c868fdd01ff37811c03e5349a06a
CoulterStutz <coultercash@gmail.com>
2024-06-13 13:49:06 -0600

    [LOOKSRARE]: Rewrite Structured Class To Be Internal

--- nftpy/LooksRare/looksrare.py
+++ nftpy/LooksRare/looksrare.py
@@ -417,3 +417,27 @@ class LooksRareAPI:
         def __repr__(self):
             return (
                 f"ListingReward(proof={self.proof}, looks_total={self.looks_total}, looks_24h={self.looks_24h}, date={self.date})")
+
+    class _TradingReward:
+        def __init__(self, proof, looks_total, looks_24h, volume_total, volume_24h, date):
+            self.proof = proof
+            self.looks_total = looks_total
+            self.looks_24h = looks_24h
+            self.volume_total = volume_total
+            self.volume_24h = volume_24h
+            self.date = date
+
+        @classmethod
+        def from_dict(cls, data):
+            proof = data.get('proof')
+            looks_total = data.get('looksTotal')
+            looks_24h = data.get('looks24h')
+            volume_total = data.get('volumeTotal')
+            volume_24h = data.get('volume24h')
+            date = data.get('date')
+            return cls(proof, looks_total, looks_24h, volume_total, volume_24h, date)
+
+        def __repr__(self):
+            return (
+                f"TradingReward(proof={self.proof}, looks_total={self.looks_total}, looks_24h={self.looks_24h}, volume_total={self.volume_total}, "
+                f"volume_24h={self.volume_24h}, date={self.date})")
