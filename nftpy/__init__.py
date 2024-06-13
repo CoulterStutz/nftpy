580ecb0a4600e26a89d14443b4177157ae1644fa
CoulterStutz <coultercash@gmail.com>
2024-06-13 14:18:22 -0600

    [PROJECT]: REFLECT VERSION CHANGES

--- nftpy/__init__.py
+++ nftpy/__init__.py
@@ -6,6 +6,6 @@ from .LooksRare import LooksRareChain, LooksRareAPI
 __all__ = ["EVM", "OpenSea", "Rarible", "Mintable", "LooksRare"]
 
 __name__ = "nftpy"
-__version__ = '1.2.2a2'
+__version__ = '1.2.2a3'
 __author__ = 'Coulter C. Stutz'
 __email__ = 'coulterstutz@gmail.com'
