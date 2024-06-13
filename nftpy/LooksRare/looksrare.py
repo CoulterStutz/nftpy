a070e49bc7790f6af23c06dde0ad29efe209c199
CoulterStutz <coultercash@gmail.com>
2024-06-13 13:34:41 -0600

    [LOOKSRARE]: Rewrite Structured Class To Be Internal

--- nftpy/LooksRare/looksrare.py
+++ nftpy/LooksRare/looksrare.py
@@ -358,3 +358,39 @@ class Order:
                 f"currency={self.currency}, amount={self.amount}, price={self.price}, nonce={self.nonce}, "
                 f"start_time={self.start_time}, end_time={self.end_time}, status={self.status}, signature={self.signature}, "
                 f"intermediary={self.intermediary}, order_type={self.order_type}, salt={self.salt}, extra_params={self.extra_params})")
+class Token:
+    def __init__(self, id, collection_address, token_id, token_uri, image_uri, is_explicit, is_animated, flag, name,
+                 description, collection, attributes):
+        self.id = id
+        self.collection_address = collection_address
+        self.token_id = token_id
+        self.token_uri = token_uri
+        self.image_uri = image_uri
+        self.is_explicit = is_explicit
+        self.is_animated = is_animated
+        self.flag = flag
+        self.name = name
+        self.description = description
+        self.collection = collection
+        self.attributes = attributes
+
+    @classmethod
+    def from_dict(cls, data):
+        id = data.get('id')
+        collection_address = data.get('collectionAddress')
+        token_id = data.get('tokenId')
+        token_uri = data.get('tokenURI')
+        image_uri = data.get('imageURI')
+        is_explicit = data.get('isExplicit')
+        is_animated = data.get('isAnimated')
+        flag = data.get('flag')
+        name = data.get('name')
+        description = data.get('description')
+        collection = data.get('collection')
+        attributes = [Attribute.from_dict(attr) for attr in data.get('attributes', [])]
+        return cls(id, collection_address, token_id, token_uri, image_uri, is_explicit, is_animated, flag, name, description, collection, attributes)
+
+    def __repr__(self):
+        return (f"Token(id={self.id}, collection_address={self.collection_address}, token_id={self.token_id}, token_uri={self.token_uri}, "
+                f"image_uri={self.image_uri}, is_explicit={self.is_explicit}, is_animated={self.is_animated}, flag={self.flag}, "
+                f"name={self.name}, description={self.description}, collection={self.collection}, attributes={self.attributes})")
